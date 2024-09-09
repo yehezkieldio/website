f82ff54cec4c3c31a54cf3c6dde3022fc0eb6b76
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-09-09 10:12:51 +0800

    feat: add favicon, vercel analytics, etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwindIntegration from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwindIntegration({
-        applyBaseStyles: false
-    })]
+  integrations: [tailwindIntegration({
+      applyBaseStyles: false
+  })],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
