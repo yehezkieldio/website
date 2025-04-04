c633506fc2e8a1ddc744c94deb8d7598bc1952a2
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-04-04 13:04:29 +0800

    chore: add prettier configuration

---
+++ prettier.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+const config = {
+    semi: true,
+    trailingComma: "none",
+    tabWidth: 4,
+    printWidth: 120,
+    bracketSpacing: true,
+    singleQuote: false,
+    arrowParens: "always",
+    quoteProps: "consistent",
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+    tailwindFunctions: ["cn"],
+    overrides: [
+        {
+            files: ["**/.vscode/*.json", "**/tsconfig.json", "**/tsconfig.*.json"],
+            options: {
+                parser: "json",
+                trailingComma: "none"
+            }
+        },
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro"
+            }
+        }
+    ]
+};
+
+export default config;
