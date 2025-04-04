069b32da56f376a7f28354edcc80d6404d29849d
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-04-04 14:30:36 +0800

    feat: add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+    site: "https://yehezkieldio.vercel.app",
     experimental: {
         contentIntellisense: true
     },
     compressHTML: false,
     vite: {
         plugins: [
+            sitemap(),
             tailwindcss(),
             mdx({
                 syntaxHighlight: "shiki",
